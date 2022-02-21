85d52ea1b47e0a9711e44a9b51cfc2bf04959269
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-02-21 17:15:37 -0500

    Added ASCII Art

--- script.py
+++ script.py
@@ -1,3 +1,4 @@
+#!/bin/env python3
 
 val = ''
 validOptions = ['cat','dog']
@@ -6,8 +7,8 @@ while val not in validOptions:
     print(','.join(validOptions))
     val = input().lower()
     if val== 'cat':
-        print('CAT ASCII ART HERE')
+        print(' _._     _,-\'\"\"`-._\n(,-.`._,\'(       |\\`-/|\n    `-.-\' \\ )-`( , o o)\n          `-    \\`_`\"\'-')
     elif val == 'dog':
-        print('DOG ASCII ART HERE')
+        print('  __      _\no\'\')}____//\n `_/      )\n (_(_/-(_/"')
     else:
         print('Invalid Response')
