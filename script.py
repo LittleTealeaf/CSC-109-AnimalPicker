da5d6e543751655c9d34057e961266d72dc4d8f7
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-02-21 17:11:05 -0500

    Repeatedly asks user to enter a valid option

--- script.py
+++ script.py
@@ -1,9 +1,13 @@
-print('Which animal would you like to see?')
-print('dog, cat')
-val = input().lower()
-if val== 'cat':
-    print('CAT ASCII ART HERE')
-elif val == 'dog':
-    print('DOG ASCII ART HERE')
-else:
-    print('Invalid Response')
+
+val = ''
+validOptions = ['cat','dog']
+while val not in validOptions:
+    print('Which animal would you like to see?')
+    print(','.join(validOptions))
+    val = input().lower()
+    if val== 'cat':
+        print('CAT ASCII ART HERE')
+    elif val == 'dog':
+        print('DOG ASCII ART HERE')
+    else:
+        print('Invalid Response')
